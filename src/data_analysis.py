02eb0cf5a42165c12f1e80fe95523864016aeb07
alekseii <dorogazm@gmail.com>
2023-12-29 23:28:55 +0300

    coverage total 77%

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from database_manager import get_historical_data
+from .database_manager import get_historical_data
 
 
 def perform_regression_analysis():
