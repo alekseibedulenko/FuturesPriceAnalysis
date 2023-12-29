87864d3cf0b70c7cf5c68da2cda55848b20e531b
alekseii <dorogazm@gmail.com>
2023-12-29 23:18:52 +0300

    Update main

--- src/main.py
+++ src/main.py
@@ -6,12 +6,17 @@ import time
 
 def main():
     # Получение модели регрессии и последнего изменения цены BTC
+    collect_data()
+    print("Получаем первые данные...")
+    time.sleep(60)
+    collect_data()
+    time.sleep(3)
     regression_model = perform_regression_analysis()
 
     while True:
-        collect_data()
         check_price_movement(regression_model)  # Передаем модель
         time.sleep(60)  # Ждем 60 секунд перед следующей проверкой
+        collect_data()
 
 
 if __name__ == "__main__":
